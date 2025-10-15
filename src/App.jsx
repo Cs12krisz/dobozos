24c1a11942587c2e5183a714c0a0ba58626d2771
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-15 11:49:41 +0200

    Szín változós komponensek

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,61 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+const App = () => {
+const [isActive, setActive] = useState(true);
+const [isDarkMode] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches);
+const backgroundColor = isActive ? "green" : "red"
+const color = isActive ? "white" : "black"
   return (
     <>
-      <div className='border'>
-        <span>Dobozok App</span>
-        <BoxComponent hatterszin="green" kiindulo={2} noveles={1}/>
-        <BoxComponent hatterszin="blue" kiindulo={3} noveles={2} />
-        <BoxComponent hatterszin="red" kiindulo={4} noveles={3}/>
-      </div>
+    <div className='border' style={{backgroundColor, color}}>
+      <ColorChangeBoxComponent isActive={isActive} ></ColorChangeBoxComponent>
+      <ButtonComponent isActive={isActive} setActive={setActive}></ButtonComponent>
+    </div>
+     App szintű state.
+    <h2>{isDarkMode ? "Sötét mód" : "Világos mód"}</h2>
     </>
+  );
+}
+
+/*
+  <CounterBoxComponent hatterszin="green" kiindulo={2} noveles={1}/>
+  <CounterBoxComponent hatterszin="blue" kiindulo={3} noveles={2} />
+  <CounterBoxComponent hatterszin="red" kiindulo={4} noveles={3}/>
+*/
+
+function ButtonComponent({isActive, setActive}) {
+  return(
+  <div style={{width : "200px", height : "200px"}}
+    className='p-2 m-5 rounded border'>
+      <button className='btn btn-success m-2 bi bi-android' disabled={isActive} onClick={() => setActive(true)}>Aktiválás</button>
+      <button className='btn btn-danger m-2 bi bi-android' disabled={!isActive} onClick={() => setActive(false)}>Inaktiválás</button>
+    </div>
   );
 }
 
 
 
-function BoxComponent({kiindulo, hatterszin, noveles}) {
+const ColorChangeBoxComponent = ({isActive}) => {
+  const color = isActive ? "green" : "red"
+
+  return(
+    <>
+    <div className="p-2 m-5 rounded border" style={{
+      backgroundColor: color,
+      width: "200px",
+      height: "200px"
+    }}>
+    </div>
+    <h2>Color change box</h2>
+    </>
+
+  );
+}
+
+
+
+const CounterBoxComponent = ({kiindulo, hatterszin, noveles}) => {
 
   const [getSzamlalo, setSzamlalo] = useState(kiindulo);
 
@@ -29,7 +68,7 @@ function BoxComponent({kiindulo, hatterszin, noveles}) {
     }}
     className='p-2 m-5 rounded'
     onClick={() => {
-      setSzamlalo(getSzamlalo + noveles);
+      setSzamlalo(getSzamlalo + noveles)
     }}
     >
       <h1>{getSzamlalo}</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import  App  from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/icons/android.svg"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
