9f477c93354158e728f2f9325f8486279b27f90a
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-15 09:41:21 +0200

    BoxComponent created and used

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='border'>
+        <span>Dobozok App</span>
+        <BoxComponent hatterszin="green" kiindulo={2} noveles={1}/>
+        <BoxComponent hatterszin="blue" kiindulo={3} noveles={2} />
+        <BoxComponent hatterszin="red" kiindulo={4} noveles={3}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+
+
+function BoxComponent({kiindulo, hatterszin, noveles}) {
+
+  const [getSzamlalo, setSzamlalo] = useState(kiindulo);
+
+  return(
+    <>
+    <div style={{
+      width: "150px",
+      height: "150px",
+      backgroundColor : hatterszin
+    }}
+    className='p-2 m-5 rounded'
+    onClick={() => {
+      setSzamlalo(getSzamlalo + noveles);
+    }}
+    >
+      <h1>{getSzamlalo}</h1>
+    </div>
+    </>
+
+  );
+}
+
+export default App;
