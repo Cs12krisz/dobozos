854a7c24e36e2ba6b5220d4ec574f96887339f58
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-19 21:33:51 +0200

    Bluebox.jsx has created

--- src/BlueBox.jsx
+++ src/BlueBox.jsx
@@ -1,5 +1,24 @@
-function BlueBox() {
-    
+import { useState } from "react";
+
+function BlueBox({buttonsClassname}) {
+    const [getSzam, setSzam] = useState(0)
+
+    return (
+        <>
+        <div style={
+            {
+                backgroundColor: "blue",
+                color: "white",
+                width: "200px",
+                height: "200px"
+            }}
+            className='p-2 m-5 rounded'>
+            <h2 style={{}} >{getSzam}</h2>
+        </div>
+        <button style={{width : "200px"}} className={buttonsClassname} onClick={() => { setSzam(getSzam + 1)}}>A szám növelése</button><br />
+        <button style={{width : "200px"}} className={buttonsClassname} onClick={() => { setSzam(getSzam - 1)}}>A szám csökkentése</button>
+        </>
+    )
 }
 
 
