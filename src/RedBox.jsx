835b7bae787b8c2f28c77ce16329b58fcfbeb9a0
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-19 21:34:24 +0200

    Redbox.jsx has created

--- src/RedBox.jsx
+++ src/RedBox.jsx
@@ -1,5 +1,24 @@
-function RedBox() {
+import { useState } from "react"
+
+function RedBox({buttonsClassname}) {
+    const [getSzam, setSzam] = useState(0)
     
+        return (
+            <>
+            <div style={
+                {
+                    backgroundColor: "red",
+                    color: "white",
+                    width: "200px",
+                    height: "200px",
+                }}
+                className='p-2 m-5 rounded'>
+                <h2>{getSzam}</h2>
+            </div>
+            <button style={{width : "200px"}} className={buttonsClassname} onClick={() => { setSzam(getSzam + 1)}}>A szám növelése</button><br />
+            <button style={{width : "200px"}} className={buttonsClassname} onClick={() => { setSzam(getSzam - 1)}}>A szám csökkentése</button>
+            </>
+        )
 }
 
 export default RedBox;
